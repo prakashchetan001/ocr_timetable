63a9d741ed6469c11f4efa2351ec66d75f9f0eab
Chetan Prakash <chprakas@microsoft.com>
2024-09-18 16:23:35 +0530

    Document Intelligence

--- src/App.js
+++ src/App.js
@@ -1,39 +1,70 @@
-import DocumentIntelligence from "@azure-rest/ai-document-intelligence";
+import {default as DocumentIntelligence , isUnexpected, getLongRunningPoller} from "@azure-rest/ai-document-intelligence";
+
+// import * as test from "@azure/ai-form-recognizer";
 import logo from './logo.svg';
 import './App.css';
 import * as React from "react";
 
 
-const OCR_ENDPOINT = "https://southeastasia.api.cognitive.microsoft.com/";
-const DOCUMENT_INTELLIGENCE_API_KEY =  "28ccc09d1747431f9e1cda90120418f9";
+// https://southeastasia.api.cognitive.microsoft.com/formrecognizer/documentModels/prebuilt-layout:analyze?api-version=2023-07-31&stringIndexType=utf16CodeUnit
+
+const OCR_ENDPOINT = "https://schedulerecongnizerus.cognitiveservices.azure.com/";
+const DOCUMENT_INTELLIGENCE_API_KEY =  "";// Replace this with the one sent on the chat
 const timetable_image=  "https://imagestorehack.blob.core.windows.net/timetables/timetable_simple.png"
 function App() {
   const documentIntelligenceRef  = React.useRef(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(undefined);
+  const [result, setResult] = React.useState(undefined);
+
+  // Image Analyzer
+  const analyzeImage = React.useCallback(async(client)=>{
+    setLoading(true);
+    const initialResponse = await client
+      .path("/documentModels/{modelId}:analyze", "prebuilt-layout")
+      .post({
+        contentType: "application/json",
+        body: {
+          urlSource:  timetable_image
+        },
+        queryParams: {
+          apiVersion: "2023-07-31",
+          stringIndexType: "utf16CodeUnit"
+        }
+       
+      });
+      console.log("**Initial Response",initialResponse);
+      if (isUnexpected(initialResponse)) {
+        console.error("The response was unexpected: ", initialResponse);
+        setError(initialResponse.body.error);
+      }
+      const poller = await getLongRunningPoller(client, initialResponse);
+      const result = (await poller.pollUntilDone()).body ;
+      setLoading(false);
+      console.log("***Logging ");
+      console.log(result);
+      setResult(result);
+  },[setError, setLoading]);
+
+
   React.useEffect(() => {
     if(!documentIntelligenceRef.current) {
       documentIntelligenceRef.current = DocumentIntelligence(OCR_ENDPOINT, {
         key:DOCUMENT_INTELLIGENCE_API_KEY,
       });
     }
-    const client = documentIntelligenceRef.current 
+    const client = documentIntelligenceRef.current;
+    analyzeImage(client);
+  }, [analyzeImage]);
 
-  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Document Intelligence</h1>
+        <div>
+          {loading && <p>Loading...</p>}
+          {error && <p>Error: {error.message}</p>}
+          {result && JSON.stringify(result.analyzeResult.tables)}  
+        </div>
     </div>
   );
 }
